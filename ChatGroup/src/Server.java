af2bd66aeac8a4e6b2697a76fd227f9749a0366f
Dante Alves <dantealves8@gmail.com>
2025-11-24 19:51:16 -0300

    feat(server): adicionar estrutura inicial do servidor

---
+++ ChatGroup/src/Server.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+}
